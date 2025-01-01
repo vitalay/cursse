04df3e78f8409f690bc7ff982ecc761a471ce8e9
vitalya <vitalay.efimov@gmail.com>
2025-01-02 01:20:20 +0500

    добавил иконки на нижний бар

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
 
 <script setup>  
 import { ref } from 'vue'
-import { CheckCircleIcon } from '@heroicons/vue/24/outline';
+import { CheckCircleIcon } from '@heroicons/vue/24/solid';
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
+
 
 </script>
 
  
 <template>
-<header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-  <a href="#">
-    <img src="./assets/img/logo.png" alt="logo" class="h-9">
-  </a>
-  <a href="#" class="text-sm">
-    <div v-if="false" class="flex items-center gap-1">
-      dey complete!
-      <CheckCircleIcon class="h-7 text-green-500" />
-    </div>
-    <div v-else class="flex items-center gap-1">
-      <div>Progress: <span class="font-mono">20%</span></div>
-      <div class="h-3 w-3 rounded-full bg-red-500"></div>
-    </div>
-  </a>
- 
-</header> 
-<main class="flex flex-grow flex-col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis reprehenderit est id porro. Doloribus, quas. Eos, error a. Error iusto est eveniet enim porro at modi voluptate numquam eaque molestiae!</main>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/img/logo.png" alt="logo" class="h-9">
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="false" class="flex items-center gap-1">
+        dey complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
 
-<nav class=" sticky  bottom-0 z-10 bg-white text-3xl">nav</nav>
+  </header>
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class=" sticky  bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a class="flex items-center justify-center p-2 text-xs capitalize" href="#Timeline">
+          <ClockIcon class="h-6 w-6" />timeline</a>
+      </li>
+      <li class="flex-1">
+        <a class="flex items-center justify-center p-2 text-xs capitalize" href="#Activities">
+          <listBulletIcon class="h-6 w-6"/>activities</a>
+      </li>
+      <li class="flex-1">
+        <a class="flex items-center justify-center p-2 text-xs capitalize" href="#Progress">
+          <ChartBarIcon class="h-6 w-6"/>progress</a>
+      </li>
+    </ul>
+  </nav>
 
 </template>
  

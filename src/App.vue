6924c192afaf82b1bf63da0e09c420df379e40a3
vitalya <vitalay.efimov@gmail.com>
2025-01-02 00:25:08 +0500

    добавил прогрес

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,10 @@ import { CheckCircleIcon } from '@heroicons/vue/24/outline';
       dey complete!
       <CheckCircleIcon class="h-7 text-green-500" />
     </div>
+    <div class="flex items-center gap-1">
+      <div>Progress: <span class="font-mono">20%</span></div>
+      <div class="h-3 w-3 rounded-full bg-red-500"></div>
+    </div>
   </a>
  
 </header> 

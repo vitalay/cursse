cb1279ca2636476a7cf8bdf33319a5154f9a0516
vitalya <vitalay.efimov@gmail.com>
2025-01-02 21:35:41 +0500

    Разметка временной шкалы

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,3 +1,16 @@
 <template>
-  <h2>Timeline</h2>
+ <div class="mt-7">
+  <ul>
+    <li v-for="i in [1, 2, 3, 4, 5]" :key="i" 
+      class="relative flex flex-col gap-2   border-t border-gray-200 py-10 px-4"
+      >
+      <a
+       href="#" 
+       class="absolute -top-4 left-1/2 -translate-x-1/2 rounded px-2 font-mono text-lg bg-gray-100 text-gray-500"
+       >
+       {{ i }}:00
+    </a>
+  </li>
+  </ul>
+ </div>
 </template>

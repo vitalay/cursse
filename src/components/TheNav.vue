24c17b5ebe09b4de0f3ad015272a790ada18c1c2
vitalya <vitalay.efimov@gmail.com>
2025-01-02 12:47:52 +0500

    Подсветка активного пункта меню

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,4 +1,5 @@
 <script setup>
+import {ref} from 'vue'
 import NavItems from './NavItems.vue';
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 
@@ -7,12 +8,19 @@ const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon,
 };
+const currentPage = ref('timeline');
+
 </script>
 
 <template>
   <nav class=" sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItems v-for="icon, page in navItems" :key="page" :href="`#${page}`">
+      <NavItems 
+       v-for="icon, page in navItems"
+       :key="page" :href="`#${page}`"
+       :class="{'bg-gray-200 pointer-events-none' : page === currentPage}"
+       @click="currentPage = page"
+       >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItems>
     </ul>

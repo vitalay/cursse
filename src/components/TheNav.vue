aaa724758de56c06088b5c21a56ca8815c8fc4bc
vitalya <vitalay.efimov@gmail.com>
2025-01-02 11:51:25 +0500

    наследование атрибутов

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,6 +1,6 @@
 <script setup>
+import NavItems from './NavItems.vue';
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
-
 
 const navItems = {
   timeline: ClockIcon,
@@ -12,11 +12,9 @@ const navItems = {
 <template>
   <nav class=" sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <a :href="`#${page}`" class="flex flex-col items-center justify-center p-2 text-xs capitalize">
-          <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
+      <NavItems v-for="icon, page in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" /> {{ page }}
+      </NavItems>
     </ul>
   </nav>
 </template>
